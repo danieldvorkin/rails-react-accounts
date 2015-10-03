632dda44d43f5934e2c496609e4eed24ab01be92
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-03 12:47:04 -0400

    Added a create method for records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,29 @@
 class RecordsController < ApplicationController
+
 	def index
+		# Render all records and store in @records
 		@records = Record.all
+	end
+
+	# Method for Record Creation
+	def create
+
+		# create a new Record, using record_params
+		@record = Record.new(record_params)
+
+		# on save 
+		if @record.save
+			# render new record data
+			render json: @record
+		else
+			render json: @record.errors, status :unprocessable_entity
+		end
+	end 
+
+	private
+
+	def record_params
+		# title, amount and date consist within a record
+		params.require(:record).permit(:title, :amount, :date)
 	end
 end
